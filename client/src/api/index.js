175c87325420fb38313d776b2e42fb42cf4f2e03
SUVENDU-CHHATOI <skc18313963@gmail.com>
2024-03-25 22:31:12 +0530

    final commit in github

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts';
+// const url = 'http://localhost:5000/posts'; // use this for running in localhost
+const url = 'https://blog-application-memories-9e68ec8a6fe4.herokuapp.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ import postRoutes from './routes/posts.js';
 const app = express();
 dotenv.config();
 
+app.get('/', (req, res) => res.send('Hello & Welcome to the site'))
+
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
@@ -17,6 +19,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 const PORT = process.env.PORT|| 5000;
+
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
